c816fc250487d9a3cc6da185b3511966e2ced3aa
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2020-05-26 00:03:38 -0300

    Nodemon and Route error tratament

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 const routeProducts = require('./src/routes/products')
 const routeOrders = require('./src/routes/orders')
+
+app.use(morgan('dev'))
+
 app.use('/products', routeProducts)
 app.use('/orders', routeOrders)
+
+app.use((req, res, next) =>{
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app
